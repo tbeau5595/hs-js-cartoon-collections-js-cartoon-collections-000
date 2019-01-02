0b2e664fabb011ec6e01d709d48299af6d72e2f6
Trey <tbeau5595@hotmail.com>
2019-01-02 19:11:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
 function dwarfRollCall(dwarves) {
+  let dwarvesNamesandNumbers = [];
+  
+  for (let i = 0; i < dwarves.length; i++) {
+     dwarvesNamesandNumbers.push([i+1] +". " + dwarves[i] + " ");
+   }
+     return dwarvesNamesandNumbers.join("");
 }
 
-function summonCaptainPlanet(planeteerCalls){
+function summonCaptainPlanet(planeteerCalls) {
+  var planet = [];
+   for (let i = 0; i < planeteerCalls.length; i++){
+     planet.push(`${planeteerCalls[i]}`.toUpperCase() + "!");
+   }
+   return planet;
+  
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4 ) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  let cheeses = ["gouda", "cheddar"]
+  for (let i = 0; i < foods.length; i++) {
+      if (foods[i] === cheeses[i]) {
+        return foods[i];
+      }
+  }
+  return ("no cheese!");
 }
